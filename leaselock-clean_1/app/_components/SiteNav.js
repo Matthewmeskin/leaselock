753e8f4f117ce42411069f4d81aee9139c790b10
Matthew Justin Meskin <100257392+Matthewmeskin@users.noreply.github.com>
2026-06-20 13:13:50 -0400

    RenterReady: update SiteNav.js

--- leaselock-clean_1/app/_components/SiteNav.js
+++ leaselock-clean_1/app/_components/SiteNav.js
@@ -3,7 +3,7 @@ export default function SiteNav() {
   return (
     <nav className="nav">
       <div className="wrap nav-inner">
-        <Link href="/" className="brand"><span className="brand-mark">🔒</span> LeaseLock</Link>
+        <Link href="/" className="brand"><span className="brand-mark">🏠</span> RenterReady</Link>
         <div className="nav-links">
           <Link href="/#how" className="hide-sm">How it works</Link>
           <Link href="/guides" className="hide-sm">Guides</Link>
